581fa27a80bcb5373b163a7399056b606243eb14
Paak0 <41326576+Paak0@users.noreply.github.com>
2019-01-25 16:19:37 +0100

    Add files via upload

--- site/script.js
+++ site/script.js
@@ -15,7 +15,7 @@ let soundNames = [
 	'formation', 'archive', 'to_player', 'healed', 'helaled', 'hp_down', 
 	'power_down', 'player_gauge'
 ];
-let adds = ['', 'a', 'b', '_a', '_b', '_mix'];
+let adds = ['', 'a', 'b', 'c', '_a', '_b', '_c', '_mix'];
 
 function changeChara(){
 	$("#optional").val("");
@@ -25,13 +25,12 @@ function changeChara(){
 	$(".link").each( function(){ $(this).attr("onclick", "" ); });
 	
 	chara = new Character();
-	chara.id = data.characters[$("#charaSelect").prop("selectedIndex") - 1].id;
-	chara.sounds = data.characters[$("#charaSelect").prop("selectedIndex") - 1].sounds;
+	chara.id = data[currentData][$("#charaSelect").prop("selectedIndex") - 1].id;
+	chara.sounds = data[currentData][$("#charaSelect").prop("selectedIndex") - 1].sounds;
 	
 	chara.sounds.forEach( elem => { $("#"+elem+"Box").css("display", "flex"); });
 	let picID = Number.isInteger(chara.id) ? 2 : 3;
 	$("head").append("<style>.container::after{ background: url(\"http://game-a.granbluefantasy.jp/assets_en/img/sp/assets/npc/zoom/" + parseInt(chara.id) + "_0" + picID + ".png\") no-repeat 50% 150px fixed; }</style>");
-	
 }
 
 function prev(id, btnId){
@@ -47,20 +46,21 @@ function next(id, btnId){
 }
 
 function getSound(name, btnId){
+	$("#button"+btnId).css("background-color", "#5252e0"); //blue
 	let index = $('#'+name).val();
 	if(parseInt(index) < 1) return;
 	
-	if(name == 'v_') index = ('000' + index).slice(-3);
+	if(name == 'v_' && index.toString().length < 3) index = ('000' + index).slice(-3);
 	if(name == 'turnend' || name == 'player_gauge') index = "";
 	
 	let link = 'http://game-a5.granbluefantasy.jp/assets/sound/voice/'+ chara.id +'_'+ name + index + $("#optional").val() +'.mp3';
 	let sound = new Audio(link);
-	
+	chara[name] = sound;
 	$("#link"+btnId).attr("onclick", "window.open("+"'"+link+"'"+")" );
 	
 	sound.onloadedmetadata = function(){
 		$("#button"+btnId).css("background-color", "#75e052"); //greenish
-		this.play();
+		chara[name].play();
 	};
 	sound.onerror = function(){
 		if((chara.id == '3040001000_03' || chara.id == '3040012000_03' || chara.id == 3040101000 || chara.id == 3040178000 || parseInt(chara.id) >= 3040180000) && $('#'+name).val().length == 1 ){
@@ -69,17 +69,16 @@ function getSound(name, btnId){
 		}
 		$("#button"+btnId).css("background-color", "#e05252"); //redish
 	}
-	chara[name] = sound;
+	
 }
 
 function playSound(name){
-	$("audio").each( function(){ this.pause(); } );
-	if(!chara[name]) return;
-	if(!isNaN(chara[name].duration)) chara[name].play();
+	if(!chara[name] || isNaN(chara[name].duration)) return;
+	chara[name].play();
 }
 
-function loadCharacters(){
-	data.characters.forEach( chara => $('#charaSelect').append($('<option>', { value: 1, text: chara.name })) );
+function loadCharacters(x){
+	data[x].forEach( ch => $('#charaSelect').append($('<option>', { value: 1, text: ch.name })) );
 }
 
 function showAllSounds(){
@@ -88,11 +87,10 @@ function showAllSounds(){
 }
 
 function updateData(){
-	// $.getJSON('https://api.myjson.com/bins/1ci0r4', function(result){
-	$.getJSON('data/characters.json', function(result){
-		data = result;
-		count = data.characters.length;
+	$("#charaSelect").children('option:not(:first)').remove();
+	$.getJSON('data/characters.json', function(res){
+		data = res;
+		count = data.ssr.length + data.sr.length + data.r.length;
 		localStorage.setItem('gbfCharacters', JSON.stringify(data));
-		loadCharacters();
 	});
 }
